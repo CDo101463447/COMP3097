b684612de3f4f3d40d55d49998d9e20e366fc1f5
CDo101463447 <christian.do@georgebrown.ca>
2025-02-21 22:19:53 -0500

    initial project

---
+++ Project/Shopping List App/Shopping List AppTests/Shopping_List_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_List_AppTests.swift
+//  Shopping List AppTests
+//
+//  Created by Christian Do on 2/20/25.
+//
+
+import Testing
+@testable import Shopping_List_App
+
+struct Shopping_List_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
