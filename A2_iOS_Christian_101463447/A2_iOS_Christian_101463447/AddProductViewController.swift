7f9b9d3a87db1ee79f29fba9eb5094d0094c9545
CDo101463447 <christian.do@georgebrown.ca>
2025-03-27 22:53:57 -0400

    Add controller for adding product view screen

---
+++ A2_iOS_Christian_101463447/A2_iOS_Christian_101463447/AddProductViewController.swift
@@ -0,0 +1,62 @@
+//
+//  AddProductViewController.swift
+//  A2_iOS_Christian_101463447
+//
+//  Created by Christian Do on 2025-03-27.
+//
+
+import UIKit
+import CoreData
+
+class AddProductViewController: UIViewController {
+
+    @IBOutlet weak var productNameTextField: UITextField!
+    @IBOutlet weak var productDescriptionTextField: UITextField!
+    @IBOutlet weak var productPriceTextField: UITextField!
+    @IBOutlet weak var productProviderTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // Save the new product
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        // Validate that all fields are filled in
+        guard let productName = productNameTextField.text, !productName.isEmpty,
+              let productDescription = productDescriptionTextField.text, !productDescription.isEmpty,
+              let productPriceText = productPriceTextField.text, !productPriceText.isEmpty,
+              let productProvider = productProviderTextField.text, !productProvider.isEmpty else {
+                  print("Please fill in all fields correctly")
+                  return
+        }
+
+        // Validate the product price as a valid number
+        guard let productPrice = Decimal(string: productPriceText) else {
+            print("Invalid price format")
+            return
+        }
+
+        // Create a new Product object
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let newProduct = Product(context: context)
+        newProduct.productName = productName
+        newProduct.productDescription = productDescription
+        newProduct.productPrice = NSDecimalNumber(decimal: productPrice)
+        newProduct.productProvider = productProvider
+
+        // Save the new product to Core Data
+        do {
+            try context.save()
+            print("Product added successfully!")
+            // Dismiss the view controller after saving
+            self.navigationController?.popViewController(animated: true)
+        } catch {
+            print("Failed to save product: \(error)")
+        }
+    }
+
+    // Cancel button to dismiss the Add Product screen
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
--- A2_iOS_Christian_101463447/A2_iOS_Christian_101463447/ProductListViewController.swift
+++ A2_iOS_Christian_101463447/A2_iOS_Christian_101463447/ProductListViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 import CoreData
 
 class ProductListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
-    @IBOutlet weak var addProductTapped: UIButton!
+
+    @IBOutlet weak var addProductTapped: UIButton! // UIButton for adding a product
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
@@ -21,8 +22,17 @@ class ProductListViewController: UIViewController, UITableViewDelegate, UITableV
         super.viewDidLoad()
         searchBar.delegate = self
         fetchProducts()
+        addProductTapped.addTarget(self, action: #selector(addProductButtonTapped), for: .touchUpInside)
     }
 
+    // Action when Add Product button is tapped
+    @objc func addProductButtonTapped() {
+        let addProductVC = AddProductViewController() // Create the AddProductViewController
+        let navigationController = UINavigationController(rootViewController: addProductVC) // Embed in navigation controller
+        self.present(navigationController, animated: true, completion: nil) // Present modally (you can also push if using a nav controller)
+    }
+
+    // Fetch the products from Core Data
     func fetchProducts() {
         let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
         
@@ -35,6 +45,7 @@ class ProductListViewController: UIViewController, UITableViewDelegate, UITableV
         }
     }
 
+    // Table view delegate and data source methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return products.count
     }
@@ -47,6 +58,7 @@ class ProductListViewController: UIViewController, UITableViewDelegate, UITableV
         return cell
     }
 
+    // Search functionality to filter products
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchText.isEmpty {
             fetchProducts()
@@ -55,6 +67,7 @@ class ProductListViewController: UIViewController, UITableViewDelegate, UITableV
         }
     }
 
+    // Filter the products based on search text
     func filterProducts(searchText: String) {
         let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "productName CONTAINS[cd] %@ OR productDescription CONTAINS[cd] %@", searchText, searchText)
