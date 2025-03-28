7c711420c08edaabb799c97ecc08158dfd50fc99
CDo101463447 <christian.do@georgebrown.ca>
2025-03-27 20:14:07 -0400

    Initial project setup with Core Data

---
+++ A2_iOS_Christian_101463447/A2_iOS_Christian_101463447/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_Christian_101463447
+//
+//  Created by Christian Do on 2025-03-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ A2_iOS_Christian_101463447/A2_iOS_Christian_101463447Tests/A2_iOS_Christian_101463447Tests.swift
@@ -0,0 +1,17 @@
+//
+//  A2_iOS_Christian_101463447Tests.swift
+//  A2_iOS_Christian_101463447Tests
+//
+//  Created by Christian Do on 2025-03-27.
+//
+
+import Testing
+@testable import A2_iOS_Christian_101463447
+
+struct A2_iOS_Christian_101463447Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
